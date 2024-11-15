74a0a7bb7c4a4ec30eba0ffd6d7ded301296df18
alinalinux <soltanicialina777@gmail.com>
2024-11-15 08:00:31 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import com.email.validator.EmailValidator;
 
 public class Main {
     public static void main(String[] args) {
-        EmailValidator emailValidator = new EmailValidator ();
-        String email1 = "user@";
 
-        System.out.println ("Has local part? " + emailValidator.hasLocalPart (email1) );
     }
 }
